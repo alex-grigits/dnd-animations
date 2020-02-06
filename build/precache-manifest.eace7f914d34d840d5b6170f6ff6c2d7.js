2d2dbc46edd9d28afd13bb5fab08663996cb9b3e
Kondratyev Yevhen <kondratyev.yevhen@gmail.com>
2020-02-06 10:28:05 +0200

    gh-pages

---
+++ build/precache-manifest.eace7f914d34d840d5b6170f6ff6c2d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e5c7435b5d1b10ca14317169b74800",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c975baad170e6316285e",
+    "url": "/static/css/main.f792624a.chunk.css"
+  },
+  {
+    "revision": "d43ab6ccf2ee7ef11ec6",
+    "url": "/static/js/2.1621aebe.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.1621aebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c975baad170e6316285e",
+    "url": "/static/js/main.448de6aa.chunk.js"
+  },
+  {
+    "revision": "9205dadeb261bd115cee",
+    "url": "/static/js/runtime-main.f96b5ba6.js"
+  }
+]);
